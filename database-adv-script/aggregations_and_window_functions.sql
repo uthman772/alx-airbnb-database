06d99e917b163c4e5019600b392c9336dcbf558f
usaman yau Muhammad <79041451+uthman772@users.noreply.github.com>
2025-09-07 07:19:52 -0700

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,1 +1,10 @@
+SELECT user_id, COUNT(*) AS total_bookings
+FROM Bookings
+GROUP BY user_id;
+
+
+SELECT property_id,
+       COUNT(*) OVER (PARTITION BY property_id) AS total_bookings,
+       RANK() OVER (ORDER BY COUNT(*) OVER (PARTITION BY property_id) DESC) AS booking_rank
+FROM Bookings;
 
