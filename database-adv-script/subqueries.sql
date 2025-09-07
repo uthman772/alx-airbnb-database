412558918d7da23a299ed648b40ddddbde37f2a9
usaman yau Muhammad <79041451+uthman772@users.noreply.github.com>
2025-09-07 13:20:56 +0000

    Add property and review selection query to subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -7,3 +7,6 @@ SELECT b.booking_id, b.start_date, u.first_name, u.last_name
 FROM Bookings b
 INNER JOIN Users u ON b.user_id = u.user_id;
 
+SELECT p.id AS property_id, p.name AS property_name, r.rating, r.comment
+FROM properties p
+LEFT JOIN reviews r ON p.id = r.property_id;
