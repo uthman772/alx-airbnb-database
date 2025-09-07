877d757b4eb2d9c895f40f1d590ffc66aa8cf4d8
usaman yau Muhammad <79041451+uthman772@users.noreply.github.com>
2025-09-07 06:50:52 -0700

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -7,6 +7,33 @@ SELECT b.booking_id, b.start_date, u.first_name, u.last_name
 FROM Bookings b
 INNER JOIN Users u ON b.user_id = u.user_id;
 
-SELECT p.id AS property_id, p.name AS property_name, r.rating, r.comment
-FROM properties p
-LEFT JOIN reviews r ON p.id = r.property_id;
+SELECT u.name AS user_name, b.id AS booking_id, b.start_date
+FROM users u
+FULL OUTER JOIN bookings b ON u.id = b.user_id;
+SELECT id, name
+FROM properties
+WHERE id IN (
+  SELECT property_id
+  FROM reviews
+  GROUP BY property_id
+  HAVING AVG(rating) > 4.0
+);
+
+SELECT u.id, u.name
+FROM users u
+WHERE (
+  SELECT COUNT(*)
+  FROM bookings b
+  WHERE b.user_id = u.id
+) > 3;
+
+
+SELECT user_id, COUNT(*) AS total_bookings
+FROM bookings
+GROUP BY user_id;
+
+SELECT property_id, COUNT(*) AS total_bookings,
+       RANK() OVER (ORDER BY COUNT(*) DESC) AS rank
+FROM bookings
+GROUP BY property_id;
+
