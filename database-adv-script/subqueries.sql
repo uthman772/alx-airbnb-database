b87e4782df7369f4e42f981dcabfb3e2f9ea8979
usaman yau Muhammad <79041451+uthman772@users.noreply.github.com>
2025-09-07 04:46:54 -0700

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,3 +1,9 @@
 SELECT b.id AS booking_id, u.name AS user_name, b.start_date, b.end_date
 FROM bookings b
 INNER JOIN users u ON b.user_id = u.id;
+
+
+SELECT b.booking_id, b.start_date, u.first_name, u.last_name
+FROM Bookings b
+INNER JOIN Users u ON b.user_id = u.user_id;
+
