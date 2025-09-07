3abc5d7c2cfa61652f22cf17b279440450abb436
usaman yau Muhammad <79041451+uthman772@users.noreply.github.com>
2025-09-07 07:06:43 -0700

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,1 +1,13 @@
+CREATE TABLE Bookings (
+  booking_id INT,
+  user_id INT,
+  property_id INT,
+  start_date DATE,
+  end_date DATE
+)
+PARTITION BY RANGE (YEAR(start_date)) (
+  PARTITION p2023 VALUES LESS THAN (2024),
+  PARTITION p2024 VALUES LESS THAN (2025),
+  PARTITION p2025 VALUES LESS THAN (2026)
+);
 
